85cf99c62d433647428be28867135c60892d4d5a
Gillian Mullen <gillian.mullen5@mail.dcu.ie>
2019-04-16 14:23:43 -0700

    Add Evaluation HOPEFUL

---
+++ test/evaluation/SemanticVisitor.java
@@ -0,0 +1,329 @@
+import java.util.*;
+
+public class SemanticVisitor implements HOPEFULVisitor {
+
+   private SymbolTable st;
+   private LinkedList<String> variablesRead;
+   private LinkedList<String> variablesWritten;
+   private LinkedList<String> functions;
+   private String scope = "global"; // always starting scope
+
+   private Boolean allVariablesWrittenTo = true;
+   private Boolean allVariablesReadFrom = true;
+   private Boolean allFuncsCalled = true;
+   private Boolean declaredBeforeUse = true;
+   private Boolean existsDuplicateVar = false;
+   private Boolean arithmeticOnIntegers = true;
+   private Boolean logicOnBooleans = true;
+   private Boolean correctAssignmentType = true;
+
+   public Object visit(SimpleNode node, Object data) {
+      throw new RuntimeException("Visit SimpleNode");
+   }
+
+   public Object visit (ASTprogram node, Object data) {
+      st = (SymbolTable) data;
+      variablesRead = st.getVariables();
+      variablesWritten = st.getVariables();
+      functions = st.getFunctions();
+
+      node.childrenAccept(this, data); // visit all children of program node
+
+      if (!variablesWritten.isEmpty()) {
+         allVariablesWrittenTo = false;
+      }
+      if (!variablesRead.isEmpty()) {
+         allVariablesReadFrom = false;
+      }
+      if (!functions.isEmpty()) {
+         allFuncsCalled = false;
+      }
+      existsDuplicateVar = st.existsDuplicate();
+
+      if (allVariablesReadFrom) {
+         System.out.println("Pass: All Variables Read From!");
+      }
+      else {
+         System.out.println("Fail: Variable(s) " + variablesRead + " Not Read From!");
+      }
+      if (allVariablesWrittenTo) {
+         System.out.println("Pass: All Variables Written To!");
+      }
+      else {
+         System.out.println("Fail: Variable(s) " + variablesWritten + " Not Written To!");
+      }
+      if (declaredBeforeUse) {
+         System.out.println("Pass: All Variables and Functions Declared Before Use!");
+      }
+      if(!existsDuplicateVar) {
+         System.out.println("Pass: All Variables Declared Only Once!");
+      }
+      else {
+         System.out.println("Fail: Duplicate Variables Defined!");
+      }
+      if (allFuncsCalled) {
+         System.out.println("Pass: All Functions Called!");
+      }
+      else {
+         System.out.println("Fail: Functions " + functions + " Called!");
+      }
+      if (correctAssignmentType) {
+         System.out.println("Pass: All Variables Assigned Correct Type!");
+      }
+      if (arithmeticOnIntegers) {
+         System.out.println("Pass: Arithmetic Expressions on Integers Only!");
+      }
+      else {
+         System.out.println("Fail: Arithmetic Expressions on Non-Integers!");
+      }
+      if (logicOnBooleans) {
+         System.out.println("Pass: Logic Expressions on Booleans Only!");
+      }
+      else {
+         System.out.println("Fail: Logic Expressions on Non-Booleans!");
+      }
+
+      return DataType.Program;
+   }
+
+   public Object visit (ASTmain node, Object data) {
+      scope = "main";
+      node.jjtGetChild(0).jjtAccept(this, data);
+      scope = "global";
+      return DataType.Main;
+   }
+
+   public Object visit (ASTfunction_declarations node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTfunction node, Object data) {
+      SimpleNode child = (SimpleNode) node.jjtGetChild(1);
+      String id = (String) child.value;
+      scope = id;
+      node.childrenAccept(this, data);
+      scope = "global";
+      return DataType.Func;
+   }
+
+   public Object visit (ASTstatement_block node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTstatement node, Object data) {
+      return node.jjtGetChild(0).jjtAccept(this, data);
+   }
+
+   public Object visit (ASTexpression node, Object data) {
+      if(node.jjtGetNumChildren() == 1) {
+         return node.jjtGetChild(0).jjtAccept(this, data);
+      }
+      else {
+         SimpleNode arg1 = (SimpleNode) node.jjtGetChild(0);
+         String type1 = arg1.toString();
+         if(type1.equals("fragment")) {
+            arg1 = (SimpleNode) arg1.jjtGetChild(0);
+            type1 = arg1.toString();
+         }
+         String id1 = "";
+
+         if(type1.equals("rhs_identifier")) {
+            id1 = (String) arg1.value;
+            type1 = st.getType(scope, id1);
+         }
+         else if(type1.equals("integer") || type1.equals("boolean")) {
+            id1 = (String) arg1.value;
+         }
+
+         SimpleNode arg2 = (SimpleNode) node.jjtGetChild(2);
+         String type2 = arg2.toString();
+         if(type2.equals("fragment")) {
+            arg2 = (SimpleNode) arg2.jjtGetChild(0);
+            type2 = arg2.toString();
+         }
+         String id2 = "";
+
+         if(type2.equals("rhs_identifier")) {
+            id2 = (String) arg2.value;
+            type2 = st.getType(scope, id2);
+         }
+         else if(type2.equals("integer") || type2.equals("boolean")) {
+            id2 = (String) arg2.value;
+         }
+
+         if(type1.equals("integer") && !type2.equals("integer")) {
+            arithmeticOnIntegers = false;
+         }
+         else if(type1.equals("boolean") && !type2.equals("boolean")) {
+            logicOnBooleans = false;
+         }
+
+         return node.childrenAccept(this, data);
+      }
+   }
+
+   public Object visit (ASTcondition node, Object data) {
+      SimpleNode child1 = (SimpleNode) node.jjtGetChild(0).jjtGetChild(0);
+      String type1 = child1.toString();
+
+      if(type1.equals("rhs_identifier")) {
+         String id = (String) child1.value;
+         type1 = st.getType(scope, id);
+      }
+
+      SimpleNode child2 = (SimpleNode) node.jjtGetChild(0).jjtGetChild(0);
+      String type2 = child1.toString();
+
+      if(type2.equals("rhs_identifier")) {
+         String id = (String) child1.value;
+         type2 = st.getType(scope, id);
+      }
+
+      if(!type1.equals("integer") && !type2.equals("integer")) {
+         arithmeticOnIntegers = false;
+      }
+
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTfragment node, Object data) {
+      return node.jjtGetChild(0).jjtAccept(this, data);
+   }
+
+   public Object visit (ASTfunction_call node, Object data) {
+      SimpleNode child = (SimpleNode) node.jjtGetChild(0);
+      String id = (String) child.value;
+      functions.remove(id);
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTinteger node, Object data) {
+      return DataType.Integer;
+   }
+
+   public Object visit (ASTstring node, Object data) {
+      return DataType.String;
+   }
+
+   public Object visit (ASTbool node, Object data) {
+      return DataType.Boolean;
+   }
+
+   public Object visit (ASTassignment node, Object data) {
+      SimpleNode child1 = (SimpleNode) node.jjtGetChild(0);
+      String id = (String) child1.value;
+      String type1 = st.getType(scope, id);
+
+      SimpleNode child2 = (SimpleNode) node.jjtGetChild(1).jjtGetChild(0);
+      String type2 = child2.toString();
+
+      if(type2.equals("fragment")) {
+         SimpleNode c2 = (SimpleNode) child2.jjtGetChild(0);
+         type2 = c2.toString();
+         if(type2.equals("rhs_identifier")) {
+            String id2 = (String) c2.value;
+            if(id2 == null) {
+               c2 = (SimpleNode) c2.jjtGetChild(0);
+               id2 = (String) c2.value;
+               System.out.println(id2);
+            }
+            type2 = st.getType(scope, id2);
+         }
+         else if(type2.equals("integer")) {
+            type2 = "integer";
+         }
+      }
+      else {
+         SimpleNode func = (SimpleNode) child2.jjtGetChild(0);
+         String funcId = (String) func.value;
+         type2 = st.getType("global", funcId);
+      }
+
+      if(!type1.equals(type2)) {
+         correctAssignmentType = false;
+         System.out.println("Fail: Variable Type " + type1 + " Assigned " + type2 + " Value!");
+         System.out.println(id + " Assigned " + child2.value + " " + node.jjtGetChild(1).jjtGetChild(0));
+      }
+
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTdeclaration node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTprint node, Object data) {
+      return node.jjtGetChild(0).jjtAccept(this, data);
+   }
+
+   public Object visit (ASTif_statement node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTwhile_loop node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTlhs_identifier node, Object data) {
+      SimpleNode parent = (SimpleNode) node.jjtGetParent();
+      String type = parent.toString();
+      String value = (String) node.value;
+      if(type.equals("assignment")) {
+         if(!st.lookup(value, scope)) {
+            declaredBeforeUse = false;
+            System.out.println("Fail: " + value + " Not Declared Before Use!");
+         }
+      }
+      variablesWritten.remove(value + scope);
+      return DataType.Id;
+   }
+
+   public Object visit (ASTrhs_identifier node, Object data) {
+      String value = (String) node.value;
+      variablesRead.remove(value + scope);
+      return DataType.Id;
+   }
+
+   public Object visit (ASTtype node, Object data) {
+      String type = (String) node.value;
+      if(type.equals("int")) {
+         return DataType.Integer;
+      }
+      else if(type.equals("boolean")) {
+         return DataType.Boolean;
+      }
+      else if(type.equals("string")) {
+         return DataType.String;
+      }
+      return DataType.TypeUnknown;
+   }
+
+   public Object visit (ASTparameter_list node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTparameter node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTargument_list node, Object data) {
+      return node.childrenAccept(this, data);
+   }
+
+   public Object visit (ASTargument node, Object data) {
+      return node.jjtGetChild(0).jjtAccept(this, data);
+   }
+
+   public Object visit (ASTarith_op node, Object data) {
+      return DataType.ArithOp;
+   }
+
+   public Object visit (ASTlogic_op node, Object data) {
+      return DataType.LogicOp;
+   }
+
+   public Object visit (ASTcomp_op node, Object data) {
+      return DataType.CompOp;
+   }
+
+}
---
+++ test/evaluation/SymbolTable.java
@@ -0,0 +1,160 @@
+import java.util.*;
+
+public class SymbolTable {
+
+   private Hashtable<String, LinkedList<String>> st;
+   private Hashtable<String, String> types;
+   private Hashtable<String, String> declTypes;
+
+   public SymbolTable() { // constructor
+      st = new Hashtable<>();
+      st.put("global", new LinkedList<>());
+      types = new Hashtable<>();
+      declTypes = new Hashtable<>();
+   }
+
+   public void createNewScope(String scope) { // creates new linkedlist for a new scope (main, func, etc)
+      st.put(scope, new LinkedList<>());
+   }
+
+   public void insert(String scope, String id, String type, String declType) { // inserts new identifier, and its type and declaration type (var/const/func), into symbol table within a specific scope
+      if (checkScope(scope) == false) {
+         createNewScope(scope);
+      }
+      LinkedList<String> currentScope = st.get(scope);
+      currentScope.add(id);
+      String key = id + scope;
+      types.put(key, type);
+      declTypes.put(id, declType);
+   }
+
+   public void insertFunc(String scope, String id, String type, String declType) { // inserts new identifier, and its type and declaration type (var/const/func), into symbol table within a specific scope
+      LinkedList<String> currentScope = st.get(scope);
+      currentScope.add(id);
+      String key = id + scope;
+      types.put(key, type);
+      declTypes.put(id, declType);
+   }
+
+   public Boolean checkScope(String scope) {
+      if(st.get(scope) != null) {
+         return true;
+      }
+      return false;
+   }
+
+   public Boolean lookup(String id, String scope) { // checks if an identifier exists within a certain scope, returns true if yes and false if no or that scope does not exist
+      if (st.get(scope) == null) {
+         return false;
+      }
+      else if(st.get(scope).contains(id)) {
+         return true;
+      }
+      return false;
+   }
+
+   public void print() { // prints out symbol table
+      String currentScope;
+      LinkedList<String> scope;
+      String key;
+
+      Enumeration e = st.keys();
+      while (e.hasMoreElements()) { // while symbol table not empty
+         currentScope = (String) e.nextElement();
+         scope = st.get(currentScope);
+         if(!scope.isEmpty()) {
+            System.out.println("Scope: " + currentScope);
+            for(int i = 0; i < scope.size(); i++) { // for each identifier within a scope
+               key = scope.get(i) + currentScope;
+               System.out.println("Declaration Type= " + declTypes.get(scope.get(i)) + ", Identifier= " + scope.get(i) + ", Variable/Return Type= " + types.get(key));
+            }
+            System.out.println();
+         }
+      }
+   }
+
+   public Boolean existsDuplicate() { // checks if there are duplicate identifiers within each scope
+      String currentScope;
+      LinkedList<String> scope;
+      String id;
+
+      Enumeration e = st.keys();
+      while (e.hasMoreElements()) { // while symbol table not empty
+         currentScope = (String) e.nextElement();
+         scope = st.get(currentScope);
+
+         while(!scope.isEmpty()) { // for each identifier within a scope
+            id = scope.pop();
+            if (scope.contains(id)) {
+               System.out.println("Error: " + id + " already defined in " + currentScope + "!");
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+
+   public String getType(String scope, String id) { // gets type of identifier (int/bool/str)
+      return types.get(id + scope);
+   }
+
+   public String getDeclType(String id) { // gets type of identifier (func/var/param)
+      return declTypes.get(id);
+   }
+
+   public String getSymbol(String scope, String id) {
+      String type = types.get(id + scope);
+      String mty;
+      if(type.equals("integer") || type.equals("int")) {
+         mty = "i32";
+      }
+      else if(type.equals("boolean")) {
+         mty = "i1";
+      }
+      else {
+         mty = "i8*";
+      }
+      return mty;
+   }
+
+   public LinkedList<String> getVariables() { // get all variables and constants
+      LinkedList<String> variables = new LinkedList<String>();
+      String key;
+      String currentScope;
+      LinkedList<String> scope;
+
+      Enumeration e = st.keys();
+      while (e.hasMoreElements()) { // while symbol table not empty
+         currentScope = (String) e.nextElement();
+         scope = st.get(currentScope);
+         for(int i = 0; i < scope.size(); i++) { // for each identifier within a scope
+            key = scope.get(i) + currentScope;
+            if (!declTypes.get(scope.get(i)).equals("FUNC")) { // if variable
+               variables.add(key);
+            }
+         }
+      }
+      return variables;
+   }
+
+   public LinkedList<String> getFunctions() { // get all declared functions
+      LinkedList<String> functions = new LinkedList<String>();
+      String key;
+      String currentScope;
+      LinkedList<String> scope;
+
+      Enumeration e = st.keys();
+      while (e.hasMoreElements()) { // while symbol table not empty
+         currentScope = (String) e.nextElement();
+         scope = st.get(currentScope);
+         for(int i = 0; i < scope.size(); i++) { // for each identifier within a scope
+            key = scope.get(i) + currentScope;
+            if (declTypes.get(scope.get(i)).equals("FUNC")) { // if function
+               functions.add(scope.get(i));
+            }
+         }
+      }
+      return functions;
+   }
+
+}
