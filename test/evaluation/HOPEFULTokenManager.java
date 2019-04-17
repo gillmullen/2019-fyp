2497d4af1eee3b6b8c83ab1e8337fefe75acb0c0
Gillian Mullen <gillian.mullen5@mail.dcu.ie>
2019-04-17 16:25:39 +0100

    Revert "Add LLVM Execution to Evaluation HOPEFUL"
    This reverts commit 5d045a7bb7b1aac14e29bbff245ebadb3fd72499

--- test/evaluation/HOPEFULTokenManager.java
+++ test/evaluation/HOPEFULTokenManager.java
@@ -3,8 +3,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 
